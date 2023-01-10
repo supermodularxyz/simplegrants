67330d027673bb86bd0d2a7ebd940879f25b4390
Eason Chai <echai2905@gmail.com>
2023-01-10 18:47:38 +0800

    feat: Add validation, documentation, and prisma hooks

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  /** Swagger (OpenAPI) for documentation */
+  const config = new DocumentBuilder()
+    .setTitle('SimpleGrants API')
+    .setDescription(
+      "This Swagger API documents the routes for SimpleGrant's backend",
+    )
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  /**
+   * Prisma interferes with NestJS enableShutdownHooks. Prisma listens for
+   * shutdown signals and will call process.exit() before your application
+   * shutdown hooks fire. To deal with this, you would need to add a listener
+   * for Prisma beforeExit event.
+   */
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  await app.listen(process.env.BACKEND_PORT);
+  console.log(`✅ Server listening on port ${process.env.BACKEND_PORT}`);
 }
 bootstrap();
