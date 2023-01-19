ed2ae08522b9e6233f87c8a25055cc636d4c8c05
Eason Chai <echai2905@gmail.com>
2023-01-19 13:12:09 +0800

    feat: Add default user role

---
+++ backend/prisma/migrations/20230119032143_set_default_user_role/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Made the column `role` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "role" SET NOT NULL,
+ALTER COLUMN "role" SET DEFAULT 'User';
