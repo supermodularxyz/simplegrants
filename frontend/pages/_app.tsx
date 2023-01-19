ee0881c21639b6caef98c4ed3221b2d1cc7e13be
Eason Chai <echai2905@gmail.com>
2023-01-19 21:45:24 +0800

    feat: Integrate fingerprint with API & add sample login flow

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { FpjsProvider } from "@fingerprintjs/fingerprintjs-pro-react";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <FpjsProvider
+      loadOptions={{
+        apiKey: process.env.NEXT_PUBLIC_FINGERPRINT_KEY || "",
+      }}
+    >
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </FpjsProvider>
   );
 }
