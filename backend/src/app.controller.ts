4943c3049927533879b9750606925f3831bb3b74
Eason Chai <echai2905@gmail.com>
2023-01-23 15:52:46 +0800

    docs: Add docs for basic healthcheck route

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({
+    description: 'Basic health check route',
+  })
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,18 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { FingerprintModule } from './fingerprint/fingerprint.module';
 import { GrantsModule } from './grants/grants.module';
+import { ProviderModule } from './provider/provider.module';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
-    FingerprintModule,
     GrantsModule,
     CacheModule.register({
       isGlobal: true,
     }),
+    ProviderModule,
   ],
   controllers: [AppController],
   providers: [AppService],
