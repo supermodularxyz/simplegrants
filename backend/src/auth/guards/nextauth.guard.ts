96d0b35c7ac2aec99c1b1853e33dbd8e4586caf3
Eason Chai <echai2905@gmail.com>
2023-01-23 14:46:32 +0800

    feat: Add role check in NextAuthGuard

--- backend/src/auth/guards/nextauth.guard.ts
+++ backend/src/auth/guards/nextauth.guard.ts
@@ -1,19 +1,25 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Inject,
-  Injectable,
-} from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { getSession } from 'next-auth/react';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Reflector } from '@nestjs/core';
+import { Role } from '@prisma/client';
+import { ROLES_KEY } from 'types/constants';
 
 @Injectable()
 export class NextAuthGuard implements CanActivate {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private reflector: Reflector,
+    private readonly prisma: PrismaService,
+  ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
     const req = context.switchToHttp().getRequest();
     const session = await getSession({ req });
 
+    // If no session exists, we don't allow any entry
     if (!session) return false;
 
     // Get user data based on session
@@ -26,6 +32,14 @@ export class NextAuthGuard implements CanActivate {
     // Append the user to the request to retrieve
     req.user = user;
 
-    return !!session && new Date() < new Date(session.expires);
+    const validSession = !!session && new Date() < new Date(session.expires);
+
+    // If no roles, just check if session is valid
+    if (!requiredRoles) {
+      return validSession;
+    }
+
+    // Otherwise, ensure that session is valid & has required roles
+    return requiredRoles.some((role) => user.role === role) && validSession;
   }
 }
