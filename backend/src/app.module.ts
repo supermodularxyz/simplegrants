90f6d9a0ed7459adad8fc13c324d3840fdf6d1d8
Eason Chai <echai2905@gmail.com>
2023-01-23 23:32:15 +0800

    chore: Update app module and dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,14 +2,12 @@ import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 import { GrantsModule } from './grants/grants.module';
 import { ProviderModule } from './provider/provider.module';
 
 @Module({
   imports: [
     UsersModule,
-    AuthModule,
     GrantsModule,
     CacheModule.register({
       isGlobal: true,
