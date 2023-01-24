60dda953ae47929cf662d633ac67de3d0980d36f
Eason Chai <echai2905@gmail.com>
2023-01-24 22:26:15 +0800

    test: Add provider tests

--- backend/src/provider/provider.service.spec.ts
+++ backend/src/provider/provider.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProviderService } from './provider.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import * as cuid from 'cuid';
+import { PaymentProvider } from '@prisma/client';
 
 describe('ProviderService', () => {
   let service: ProviderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [ProviderService],
+      exports: [ProviderService],
     }).compile();
 
     service = module.get<ProviderService>(ProviderService);
@@ -14,5 +19,27 @@ describe('ProviderService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return the default provider', async () => {
+    // Creating a mock result
+    const result: PaymentProvider = {
+      id: cuid(),
+      name: 'Default Provider',
+      type: 'CARD',
+      acceptedCountries: ['US', 'MY'],
+      denominations: ['USD'],
+      website: 'https://stripe.com/docs/api',
+      schema: {},
+      version: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    jest
+      .spyOn(service, 'getProvider')
+      .mockImplementation(async () => await result);
+
+    expect(await service.getProvider()).toBe(result);
   });
 });
