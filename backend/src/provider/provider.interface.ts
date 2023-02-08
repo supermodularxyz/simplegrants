9bf811536e7dcdf5c26cfae4aa6724557eb1eadb
Eason Chai <echai2905@gmail.com>
2023-02-08 19:05:13 +0800

    docs: Update API response for checkout info

--- backend/src/provider/provider.interface.ts
+++ backend/src/provider/provider.interface.ts
@@ -1,5 +1,18 @@
+import { ApiResponseProperty } from '@nestjs/swagger';
+
 export class SuccessfulCheckoutInfo {
+  @ApiResponseProperty({
+    type: Number,
+  })
   donated: number;
+
+  @ApiResponseProperty({
+    type: Number,
+  })
   matched: number;
+
+  @ApiResponseProperty({
+    type: Number,
+  })
   numberOfGrants: number;
 }
