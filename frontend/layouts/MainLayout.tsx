7c911f5a9d56e6800a1cb16f5ace9716f819421e
Eason Chai <echai2905@gmail.com>
2023-02-15 23:27:52 +0800

    feat: Add basic padding to MainLayout

--- frontend/layouts/MainLayout.tsx
+++ frontend/layouts/MainLayout.tsx
@@ -10,7 +10,7 @@ export default function MainLayout({ children, className }: IMainLayoutProps) {
   return (
     <main
       className={clsx(
-        "flex flex-col min-w-screen min-h-screen w-full h-full",
+        "flex flex-col min-w-screen min-h-screen w-full h-full px-2 py-4 lg:px-8 lg:py-6",
         className
       )}
     >
