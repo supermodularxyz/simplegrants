1ab21656d150a3dc27990a700e59475a7f2c2beb
Eason Chai <echai2905@gmail.com>
2023-02-15 22:54:09 +0800

    feat: Allow MainLayout to be customized

--- frontend/layouts/MainLayout.tsx
+++ frontend/layouts/MainLayout.tsx
@@ -1,15 +1,19 @@
-import Head from "next/head";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { useVisitorData } from "@fingerprintjs/fingerprintjs-pro-react";
-import React, { PropsWithChildren, ReactNode } from "react";
+import clsx from "clsx";
+import React, { ReactNode } from "react";
 
 interface IMainLayoutProps {
   children: ReactNode;
+  className?: string;
 }
 
-export default function MainLayout({ children }: IMainLayoutProps) {
+export default function MainLayout({ children, className }: IMainLayoutProps) {
   return (
-    <main className="flex flex-col min-w-screen min-h-screen w-full h-full items-center justify-center">
+    <main
+      className={clsx(
+        "flex flex-col min-w-screen min-h-screen w-full h-full",
+        className
+      )}
+    >
       {children}
     </main>
   );
