4964fd9b9186d0f0f702e14e2ed05dd2851734bd
Eason Chai <echai2905@gmail.com>
2023-02-15 23:29:31 +0800

    feat: Add basic navbar functionality

--- frontend/layouts/Navbar.tsx
+++ frontend/layouts/Navbar.tsx
@@ -72,16 +72,13 @@ export default function Navbar({ children }: INavbarProps) {
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
+                <a className="justify-between">Profile</a>
               </li>
               <li>
                 <a>Settings</a>
               </li>
               <li>
-                <a>Logout</a>
+                <a onClick={() => signOut()}>Logout</a>
               </li>
             </ul>
           </div>
