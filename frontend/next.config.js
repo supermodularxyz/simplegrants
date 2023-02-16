c728d987235bb0d5910f7b3d8e36360c12af1db4
Eason Chai <echai2905@gmail.com>
2023-02-16 23:58:32 +0800

    chore: Add remote patterns for nextjs image rendering

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
