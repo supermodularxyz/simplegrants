3e8f3d7014b7deefed325e06479975e677e580f8
Eason Chai <echai2905@gmail.com>
2023-02-17 11:52:27 +0800

    feat: Add input and debounce it

---
+++ frontend/components/Input.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import clsx from "clsx";
+import { debounce } from "lodash";
+
+interface IInputProps {
+  type?: React.HTMLInputTypeAttribute;
+  placeholder?: string;
+  className?: string;
+  onChange: (value: string) => void;
+}
+
+export default function Input({
+  type,
+  placeholder,
+  className,
+  onChange,
+}: IInputProps) {
+  const debouncedOnChange = React.useCallback(
+    debounce((value) => {
+      onChange(value);
+    }, 300),
+    [onChange]
+  );
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedOnChange(event.target.value);
+  };
+
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      className={clsx("input input-bordered w-full max-w-xs", className)}
+      onChange={handleChange}
+    />
+  );
+}
