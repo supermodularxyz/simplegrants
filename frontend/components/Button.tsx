b676d52cdbe9704b15a26503594f74c4af33b2c8
Eason Chai <echai2905@gmail.com>
2023-02-17 21:56:14 +0800

    feat: Add disabled styling for button

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -5,16 +5,23 @@ interface IButtonProps {
   children: ReactNode;
   onClick?: (e?: any) => any;
   className?: string;
+  disabled?: boolean;
 }
 
-export default function Button({ children, onClick, className }: IButtonProps) {
+export default function Button({
+  children,
+  onClick,
+  className,
+  disabled = false,
+}: IButtonProps) {
   return (
     <button
       onClick={onClick}
       className={clsx(
-        "btn-primary font-bold text-lg px-12 py-3 w-full rounded-full text-sg-900",
+        "btn-primary font-bold text-lg px-12 py-3 w-full rounded-full text-sg-900 disabled:bg-sg-50 disabled:text-sg-500",
         className
       )}
+      disabled={disabled}
     >
       {children}
     </button>
