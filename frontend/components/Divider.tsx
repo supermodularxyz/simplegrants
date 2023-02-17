a79b99ca2355230889658e4b9362fc87c0cb212e
Eason Chai <echai2905@gmail.com>
2023-02-17 21:55:59 +0800

    feat: Add divider styling via classname

--- frontend/components/Divider.tsx
+++ frontend/components/Divider.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import * as Separator from "@radix-ui/react-separator";
+import clsx from "clsx";
 
 interface IDividerProps {
   orientation: "vertical" | "horizontal";
+  className?: string;
 }
 
-const Divider = ({ orientation }: IDividerProps) => (
+const Divider = ({ orientation, className }: IDividerProps) => (
   <Separator.Root
-    className="bg-sg-900 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-10 data-[orientation=vertical]:w-px"
+    className={clsx(
+      "bg-sg-900 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-10 data-[orientation=vertical]:w-px",
+      className
+    )}
     decorative
     orientation={orientation}
   />
