e90724428ccd5b83a200d7067d8d79d1d65abd18
Eason Chai <echai2905@gmail.com>
2023-02-19 20:16:44 +0800

    feat: Update axios to use API_URL env

--- frontend/utils/axios.ts
+++ frontend/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 5000,
   withCredentials: true,
 });
