cfe954141f77cd95a1c81c7ffdd5c546795fe84d
Eason Chai <echai2905@gmail.com>
2023-02-19 22:53:09 +0800

    fix: Fix layout issues and images

--- frontend/layouts/landing/LandingNavbar.tsx
+++ frontend/layouts/landing/LandingNavbar.tsx
@@ -1,5 +1,5 @@
-import { useSession, signOut, signIn } from "next-auth/react";
-import React, { ReactNode } from "react";
+import { signIn } from "next-auth/react";
+import React from "react";
 import Link from "next/link";
 import clsx from "clsx";
 import Image from "next/image";
@@ -10,8 +10,6 @@ interface ILandingNavbarProps {
 }
 
 export default function LandingNavbar({ className }: ILandingNavbarProps) {
-  const { data: session } = useSession();
-
   return (
     <div className={clsx("navbar", className)}>
       <div className="navbar-start w-max">
@@ -27,47 +25,43 @@ export default function LandingNavbar({ className }: ILandingNavbarProps) {
         </div>
       </div>
       <div className="navbar-end flex-grow">
-        {session && (
-          <>
-            <div className="flex-none gap-x-7 hidden lg:flex">
-              <Button style="ghost" onClick={() => signIn()}>
-                Sign In
-              </Button>
-              <Button className="bg-white border-none" onClick={() => signIn()}>
-                Sign Up
-              </Button>
-            </div>
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-              >
-                <li>
-                  <Link href="/sign-in">Sign In</Link>
-                </li>
-                <li>
-                  <Link href="/sign-in">Sign Up</Link>
-                </li>
-              </ul>
-            </div>
-          </>
-        )}
+        <div className="flex-none gap-x-7 hidden lg:flex">
+          <Button style="ghost" onClick={() => signIn()}>
+            Sign In
+          </Button>
+          <Button className="bg-white border-none" onClick={() => signIn()}>
+            Sign Up
+          </Button>
+        </div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link href="/sign-in">Sign In</Link>
+            </li>
+            <li>
+              <Link href="/sign-in">Sign Up</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "picsum.photos",
+        hostname: "*.picsum.photos",
       },
     ],
   },
