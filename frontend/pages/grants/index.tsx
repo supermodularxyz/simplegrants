c585a94472d5ac49bbb8a24f934faac94c9dffa2
Eason Chai <echai2905@gmail.com>
2023-02-22 00:34:37 +0800

    chore: Update GrantCard button to be hidden when not signed in

--- frontend/pages/grants/index.tsx
+++ frontend/pages/grants/index.tsx
@@ -148,6 +148,7 @@ export default function Grants() {
               {data &&
                 data.map((grant) => (
                   <GrantCard
+                    hideButton={!session}
                     grant={grant}
                     onClick={() => router.push(`/grants/${grant.id}`)}
                     key={grant.id}
