b3cc90275b9ff6a06a72af61db6306954fb5be0e
Eason Chai <echai2905@gmail.com>
2023-04-02 00:30:12 +0800

    feat: Explicitly ensure all non whitelisted properties are stripped during calls

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.enableCors({
     origin:
