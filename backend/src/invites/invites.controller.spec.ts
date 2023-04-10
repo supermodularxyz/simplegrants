8cdedb2cecd9c9908317b703b6844441aa880788
Eason Chai <echai2905@gmail.com>
2023-04-11 00:22:17 +0800

    tests: Add tests for invites

--- backend/src/invites/invites.controller.spec.ts
+++ backend/src/invites/invites.controller.spec.ts
@@ -1,18 +1,76 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InvitesController } from './invites.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { InvitesService } from './invites.service';
+import { invitesService, prismaService, users } from 'test/fixtures';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CacheModule } from '@nestjs/common';
+import { UserProfile } from 'src/users/users.interface';
 
 describe('InvitesController', () => {
   let controller: InvitesController;
+  let service: InvitesService;
+  let user: UserProfile;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        CacheModule.register({
+          isGlobal: true,
+        }),
+        ThrottlerModule.forRoot({
+          ttl: 60,
+          limit: 10,
+        }),
+      ],
+      providers: [
+        {
+          provide: APP_GUARD,
+          useClass: ThrottlerGuard,
+        },
+        {
+          provide: PrismaService,
+          useValue: prismaService,
+        },
+        {
+          provide: InvitesService,
+          useValue: invitesService,
+        },
+      ],
       controllers: [InvitesController],
     }).compile();
 
     controller = module.get<InvitesController>(InvitesController);
+    service = module.get<InvitesService>(InvitesService);
+
+    [user] = users;
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('claimInviteCode', () => {
+    it('should call the service function appropriately', async () => {
+      const code = 'clfsbx7bb0000pd0kjloobn8a';
+      await controller.claimInviteCode(code, {
+        user,
+      });
+
+      expect(service.claimInviteCode).toHaveBeenCalledWith(code, user);
+    });
+
+    it('should return the correct value', async () => {
+      const code = 'clfsbx7bb0000pd0kjloobn8a';
+      const result = await controller.claimInviteCode(code, {
+        user,
+      });
+
+      expect(result).toEqual({
+        code,
+        claimed: true,
+      });
+    });
   });
 });
--- backend/src/invites/invites.service.spec.ts
+++ backend/src/invites/invites.service.spec.ts
@@ -1,18 +1,89 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InvitesService } from './invites.service';
+import { CacheModule, HttpException, HttpStatus } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
+import {
+  claimedCode,
+  claimedInviteCode,
+  inviteCode,
+  prismaService,
+  users,
+} from 'test/fixtures';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserProfile } from 'src/users/users.interface';
+import { GrantSortOptions } from 'src/grants/grants.interface';
 
 describe('InvitesService', () => {
   let service: InvitesService;
+  let prisma: PrismaService;
+  let userA: UserProfile;
+  let userB: UserProfile;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InvitesService],
+      imports: [
+        CacheModule.register({
+          isGlobal: true,
+        }),
+        ThrottlerModule.forRoot({
+          ttl: 60,
+          limit: 10,
+        }),
+      ],
+      providers: [
+        InvitesService,
+        {
+          provide: PrismaService,
+          useValue: prismaService,
+        },
+      ],
     }).compile();
 
     service = module.get<InvitesService>(InvitesService);
+    prisma = module.get<PrismaService>(PrismaService);
+    [userA, userB] = users;
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('claimInviteCode', () => {
+    it('should throw an error if ecosystem builder already exists', async () => {
+      await expect(service.claimInviteCode('code', userA)).rejects.toThrow(
+        new HttpException(
+          'User already has an ecosystem builder account. Please login to the ecosystem builder platform.',
+          HttpStatus.BAD_REQUEST,
+        ),
+      );
+    });
+
+    it('should allow user to claim invite code', async () => {
+      jest.spyOn(prisma.ecosystemBuilder, 'findUnique').mockResolvedValue(null);
+      expect(await service.claimInviteCode('inviteCodeId', userB)).toEqual(
+        claimedInviteCode,
+      );
+    });
+
+    it('throw error if invite code is not found', async () => {
+      jest.spyOn(prisma.inviteCodes, 'findUnique').mockResolvedValue(null);
+      await expect(service.claimInviteCode('code', userA)).rejects.toThrow(
+        new HttpException('Invalid invite code', HttpStatus.NOT_FOUND),
+      );
+    });
+
+    it('throw error if invite code has been claimed before', async () => {
+      jest.spyOn(prisma.inviteCodes, 'findUnique').mockResolvedValue({
+        ...inviteCode,
+        claimed: true,
+      });
+
+      await expect(service.claimInviteCode('code', userA)).rejects.toThrow(
+        new HttpException(
+          'Invite code already claimed',
+          HttpStatus.UNPROCESSABLE_ENTITY,
+        ),
+      );
+    });
   });
 });
